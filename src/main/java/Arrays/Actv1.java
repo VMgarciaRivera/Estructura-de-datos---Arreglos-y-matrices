f021f4678cc97a4e15b574416a69d464c4832f05
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-03-25 15:02:16 -0500

    Add method to fill array with random values and return it

--- src/main/java/Arrays/Actv1.java
+++ src/main/java/Arrays/Actv1.java
@@ -3,15 +3,18 @@
 //INICIALIZARLO CON VALORES ALEATORIOS
 
 package Arrays;
+import java.util.Arrays;
 
 public class Actv1 {
 
     public int[] arreglo = new int[10];
 
-    public void llenarArreglo(){
+    public int[] llenarArreglo(){
         for (int i = 0; i < arreglo.length; i++) {
             arreglo[i] = (int)(Math.random()*100);
         }
+        System.out.println("Arreglo creado: " + Arrays.toString(arreglo));
+        return arreglo;
     }
 
 }
