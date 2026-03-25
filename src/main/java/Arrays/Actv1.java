dace767b829689c0c226885f5ba2cb09c0692c87
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-03-25 14:10:58 -0500

    Actividad - declaracion y creacion de un arreglo

---
+++ src/main/java/Arrays/Actv1.java
@@ -0,0 +1,17 @@
+//DECLARACION Y CREACION DE UN ARREGLO
+//DELCARAR UN ARREGLO DE 10 ENTEROS
+//INICIALIZARLO CON VALORES ALEATORIOS
+
+package Arrays;
+
+public class Actv1 {
+
+    public int arreglo [] = new int[10];
+
+    public void llenarArreglo(){
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = (int)(Math.random()*100);
+        }
+    }
+
+}
