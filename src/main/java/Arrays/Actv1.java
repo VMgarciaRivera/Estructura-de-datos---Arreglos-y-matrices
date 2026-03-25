ec82ff1bed8bf1352dea7cef44394ae330081585
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-03-25 14:16:43 -0500

    Refactor array declaration in Actv1

--- src/main/java/Arrays/Actv1.java
+++ src/main/java/Arrays/Actv1.java
@@ -6,7 +6,7 @@ package Arrays;
 
 public class Actv1 {
 
-    public int arreglo [] = new int[10];
+    public int[] arreglo = new int[10];
 
     public void llenarArreglo(){
         for (int i = 0; i < arreglo.length; i++) {
