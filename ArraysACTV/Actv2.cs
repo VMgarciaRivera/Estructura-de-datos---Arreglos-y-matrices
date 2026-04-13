060930a5b2a12989406398bf09cb1a448b762877
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-04-12 21:52:50 -0500

    feat: recorrer el array con bucles

---
+++ ArraysACTV/Actv2.cs
@@ -0,0 +1,23 @@
+//RECORRER EL ARRAY DE Actv1 USANDO UN BLUCE FOR CLASICO
+//RECORRER EL ARRAY DE Actv1 USANDO UN BLUCE FOR-EACH
+
+class Actv2
+{
+    public static void RecorrerArregloFor(int[] arreglo)
+    {
+        Console.WriteLine("Recorriendo el arreglo con un bucle for clásico:");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.WriteLine(arreglo[i]);
+        }
+    }
+
+    public static void RecorrerArregloForEach(int[] arreglo)
+    {
+        Console.WriteLine("Recorriendo el arreglo con un bucle foreach:");
+        foreach (int numero in arreglo)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
