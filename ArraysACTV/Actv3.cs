bac105695a5ac08bc72630a243df784ac1d01720
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-04-12 21:57:02 -0500

    feat(arrays): Modificación de array

---
+++ ArraysACTV/Actv3.cs
@@ -0,0 +1,29 @@
+/*Modificación
+Cambiar todos los valores impares por cero.
+Multiplicar todos los valores por su índice.
+*/
+
+class Actv3
+{
+    public static void CambiarImparesPorCero(int[] arreglo)
+    {
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            // Cambiar valores impares por cero
+            if (arreglo[i] % 2 != 0)
+            {
+                arreglo[i] = 0;
+            }
+        }
+    }
+
+    public static void MultiplicarPorIndice(int[] arreglo)
+    {
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            // Multiplicar cada valor por su índice
+            arreglo[i] *= i;
+        }
+    }
+
+}
